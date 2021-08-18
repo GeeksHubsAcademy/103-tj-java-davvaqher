8602da99636aac68d6e0292912d21325d68a1c7b
Manuel S. Lemos <manuelslemos@gmail.com>
2021-08-18 10:20:50 +0200

    Revert "Actualizacion arbol.java"

--- src/main/java/es/geekshubs/academy/Arbol.java
+++ src/main/java/es/geekshubs/academy/Arbol.java
@@ -1,7 +1,6 @@
 package es.geekshubs.academy;
 
 import java.util.Collections;
-import java.util.Iterator;
 
 public class Arbol {
 
@@ -10,37 +9,8 @@ public class Arbol {
    * @param input
    * @return
    */
-	public String apply(final Integer input) {
-		  String result = "";
-		  System.out.println("lado: "+input);
-		  
+  public String apply(final Integer input) {
+      return null;
+  }
 
-		for (int i = 1;i <= input;i++) {
-			result += " ";
-			for(int j = 1;j <=input-i;j++) {
-				result += " ";
-			}
-			for(int j = 1;j < i;j++) {
-				result += "*";
-			}
-			result += "\n";
-			
-		}// fin for
-		  
-		  result += lado(input); // crea la ultima fila desde el metodo
-		  System.out.println(result);
-		  
-	      return result;
-    }// fin aplly
-	
-	public String lado(int input) { // metodo crea una fila con el numero de "*" dado
-		String lado="";
-		for (int i = 0; i < input; i++) { //FILA
-			
-			lado += "*";
-		}
-		
-		return lado;
-	}
-
-}// fin clase
+}
